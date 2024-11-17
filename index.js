eb5c6bd687fd4246f82216cf04221a096230cceb
Ciugiu <153821557+Ciugiu@users.noreply.github.com>
2024-11-17 17:46:38 +0100

    router

---
+++ index.js
@@ -0,0 +1,24 @@
+import usersRouter from "./routes/users.js";
+import express, { json } from "express";
+
+const app = express();
+const port = 3000;
+
+console.log("test");
+
+// MIDDLEWARE
+app.use(express.json());
+// users endpoint
+app.use("/api/", usersRouter);
+
+// HOME GET METHOD
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to my users API ! 🎉" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+//Open your browser and go to http://localhost:3000.
+// You should see the message "Welcome to our simple REST API!"
